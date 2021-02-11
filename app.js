89f54f23f07a12d5a511d200265e43045dbdd5e2
unknown <firmansyahken4@gmail.com>
2021-02-12 06:53:27 +0700

    My Template

--- app.js
+++ app.js
@@ -1,7 +1,55 @@
 const navbar = document.querySelector(".navbar")
-const banner = document.querySelector(".banner")
-window.addEventListener("scroll", stickyNav)
 
+window.addEventListener("scroll", stickyNav)
 function stickyNav() {
     navbar.classList.toggle("active", scrollY > 0)
+}
+
+$(".scroll").on("click", Scroll)
+
+function Scroll() {
+    let target = this.dataset.target
+    $(".nav-link").removeClass("active")
+    $(this).addClass("active")
+    $("html, body").animate({
+      scrollTop: $(target).offset().top -80
+    }, 10, "swing")
+  
+}
+
+let items = []
+
+$("#cari").on("keyup", searchingData)
+
+function searchingData() {
+    let query = this.value.toLowerCase()
+    let search = items.filter(item => {
+        return (item.name.toLowerCase().includes(query))
+    })
+    renderData(search)
+}
+
+async function getData() {
+    let data = await fetch("data.json")
+    let vouchers = await data.json()
+    items = vouchers.data
+    renderData(vouchers.data)
+    
+}
+
+getData()
+
+function renderData(data) {
+    let templates = data.map(voucher => {
+        return `
+            <div class="col-4 col-lg-3">
+                <div class="overlay">
+                    <img src="${voucher.image}" class="image">
+                    <div class="mid"><p>${voucher.name}</p></div>
+                </div>
+            </div>        
+        `
+    }).join('')
+    $(".vouchers").html(templates)
+
 }
